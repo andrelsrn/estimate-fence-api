6260145b24186aa7f7ccddffb3c5709e107fb370
andrelsrn <andrel.srn@hotmail.com>
2025-11-17 12:06:39 -0300

    Project created

---
+++ src/test/java/com/orcamento/cerca/OrcamentoCercaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcamento.cerca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoCercaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
