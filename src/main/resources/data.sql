0fb0e079c107247ff84a1f829560b4ae782f39de
andrelsrn <andrel.srn@hotmail.com>
2026-01-13 17:24:15 -0300

    add initial seed data for price table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+-- =========================
+-- TABELA DE PREÇOS (SEED)
+-- =========================
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('6x6', 'branco', 25.00, 'pvc');
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('6x8', 'branco', 22.00, 'pvc');
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('6x6', 'madeira', 20.00, 'madeira');
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('6x8', 'madeira', 18.00, 'madeira');
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('5x6', 'preto', 28.00, 'aluminio');
+
+INSERT INTO tabela_preco (tamanho_painel, cor, preco_por_metro_linear, material)
+VALUES ('6x6', 'preto', 30.00, 'aluminio');
